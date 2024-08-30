89074e97bedc7a8494423a6a899a597ae6e23b11
YousifFadhel <yousifmaher04@gmail.com>
2024-08-29 22:35:53 -0400

    DarkAndLightMode

--- js/script.js
+++ js/script.js
@@ -1,42 +1,9 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-});
 
-navbarLinks.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-});
-
-
-const lightModeButton = document.getElementById('light-mode-button');
-const darkModeButton = document.getElementById('dark-mode-button');
-
-// Function to switch to light mode
-
-function switchToLightMode() {
-  document.body.classList.remove('dark-mode');
-  localStorage.setItem('theme', 'light');
+if (toggleButton && navbarLinks) {
+  toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+  });
 }
-
-
-// Function to switch to dark mode
-function switchToDarkMode() {
-  document.body.classList.add('dark-mode');
-  localStorage.setItem('theme', 'dark');
-}
-
-// Event listeners for the buttons
-lightModeBtn.addEventListener('click', switchToLightMode);
-darkModeBtn.addEventListener('click', switchToDarkMode);
-
-// Check and apply the saved theme on page load
-window.onload = function() {
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark') {
-      switchToDarkMode();
-  } else {
-      switchToLightMode();
-  }
-};
